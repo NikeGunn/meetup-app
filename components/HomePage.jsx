8af8f9e3acbce588dc8ec2ce623070ec5b6e93f8
Nikhil Bhagat <carryonprogrammer@gmail.com>
2024-04-03 16:33:17 +0545

    improved UI

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -1,25 +1,56 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Button, Container, TextField, Typography, Link } from "@mui/material";
 
 const HomePage = () => {
   const [input, setInput] = useState("");
   const navigate = useNavigate();
+
   const submitHandler = () => {
     navigate(`/room/${input}`);
   };
 
   return (
-    <div>
-      <div>
-        <input
+    <Container maxWidth="sm">
+      <Typography variant="h2" align="center" gutterBottom>
+        Quick Meet
+      </Typography>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          submitHandler();
+        }}
+      >
+        <TextField
           value={input}
           onChange={(e) => setInput(e.target.value)}
-          type="text"
-          placeholder="Enter your name to join meeting..."
+          label="Enter Random Number to Join Meeting"
+          fullWidth
+          variant="outlined"
+          name="name"
+          required
+          margin="normal"
+          InputProps={{ style: { fontSize: 20 } }}
         />
-        <button onClick={submitHandler}>Join Meeting</button>
-      </div>
-    </div>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          fullWidth
+          size="large"
+          sx={{ mt: 2 }}
+        >
+          Join Meeting
+        </Button>
+      </form>
+      {/* Footer */}
+      <Typography mt={4} variant="body2" color="textSecondary" align="center">
+        Created with ❤️ by{" "}
+        <Link href="https://github.com/NikeGunn" target="_blank" rel="noopener">
+          Nikhil Bhagat
+        </Link>
+      </Typography>
+    </Container>
   );
 };
 
